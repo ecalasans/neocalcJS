fe6581c2952d3d5ee40469b944c7ce582e3b595e
Eric Calasans <ericcalasans@gmail.com>
2019-01-15 21:41:49 -0300

    Sets e gets de eletrólitos, volumes de eletrólitos feitos

--- classes.js
+++ classes.js
@@ -4,8 +4,25 @@ var hv = {
     "peso" : 0,
     "vig" : 0,
     "volume" : 0,
-    //Sets e gets
+    "na" : 0,
+    "k" : 0,
+    "ca" : 0,
+    "mg" : 0,
 
+    //Concentrações de eletrólitos
+    "na20" : 3.4, //em mEq/mL
+    "ca10" : 9.0, //em mg/mL
+    "conc_k" : 0,
+    "setCK" : function (sel_k) {
+        this.conc_k = sel_k;
+    },
+    "conc_mg" : 0,
+    "setCMg" : function (sel_mg) {
+        this.conc_mg = sel_mg;
+    },
+
+
+    //Sets e gets
     "setPeso" : function (peso) {
         this.peso = peso;
     },
@@ -24,13 +41,52 @@ var hv = {
     "getVolume" : function () {
         return this.volume;
     },
+    "setNa" : function (dose) {
+        this.na = dose;
+    },
+    "getNa" : function () {
+        return this.na;
+    },
+    "setCa" : function (dose) {
+        this.ca = dose;
+    },
+    "getCa" : function () {
+        return this.ca;
+    },
+    "setK" : function (dose) {
+        this.k = dose;
+    },
+    "getK" : function () {
+        return this.k;
+    },
+    "setMg" : function (dose) {
+        this.mg = dose;
+    },
+    "getMg" : function () {
+        return this.mg;
+    }
+,
+
+    //Cálculo do volume de eletrólitos
+    "volNa" : function () {
+        return this.peso * this.na / this.na20;
+    },
+    "volCa" : function () {
+        return this.peso * this.ca;
+    },
+    "volK" : function () {
+        return this.peso * this.k / this.conc_k;
+    },
+    "volMg" : function () {
+        return this.peso * this.mg / this.conc_mg;
+    },
 
     //Volume de cálculo
     "volumeCalculo" : function () {
         return this.peso * this.volume;
     },
 
-    //Concentração
+    //Concentração Teórica
     "concentracaoTeorica" : function () {
         var gr_glicose = 1.44 * this.peso * this.vig;
         return (100 * gr_glicose) / this.volumeCalculo();
@@ -59,8 +115,6 @@ var hv = {
         }
         return resultado;
     }
-
-
 
 
 }
